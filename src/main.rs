9eb396815c566fd7b10a770fa1a5c1d998b7f253
Bishree <ahmedbishree@gmail.com@users.noreply.github.com>
2021-04-16 21:27:42 +0300

    -guessing_game_01

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+mod test;
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+
+    //create the secret num
+   let secret = rand::thread_rng().gen_range(1,101);
+
+    //make a loop
+    loop {
+        println!("enter your guess number");
+        let mut guess = String::new();
+        io::stdin().read_line(& mut guess).expect("this is not a number");
+        let guess:u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+        println!("your guess number is {}", guess);
+        match guess.cmp(&secret) {
+            Ordering::Greater => println!("To Big try again"),
+            Ordering::Less => println!("To Small try again"),
+            Ordering::Equal => {
+                println!(" You WIN ");
+                break;
+            },
+        }
+    }
+}

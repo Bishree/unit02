7df390b110cbc53cf4aadb3c71c83c66d1de92ab
Bishree <ahmedbishree@gmail.com@users.noreply.github.com>
2021-04-20 18:27:28 +0300

    -guessing_game_01

--- src/main.rs
+++ src/main.rs
@@ -6,27 +6,31 @@ use rand::Rng;
 mod test;
 
 fn main() {
-
-    //create the secret num
-    let secret = rand::thread_rng().gen_range(1, 101);
-
-    //make a loop
+    //creat a secret num
+    let mut secret = rand::thread_rng().gen_range(1,101);
+    //make a loop to hole process
     loop {
-        println!("enter your guess number");
+        //capture user input
         let mut guess = String::new();
-        io::stdin().read_line(&mut guess).expect("this is not a number");
-        let guess: u32 = match guess.trim().parse() {
+        io::stdin().read_line(& mut guess).expect("this is not a num");
+        //match user input to number
+        let guess:u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_)=> {
+                println!("this is not a number");
+                continue;
+            }
         };
-        println!("your guess number is {}", guess);
-        match guess.cmp(&secret) {
-            Ordering::Greater => println!("To Big try again"),
-            Ordering::Less => println!("To Small try again"),
+        // match the correct number
+        match guess.cmp(& mut secret) {
+            Ordering::Less => println!("Too Small Try again"),
+            Ordering::Greater => println!("Too Big Try again"),
             Ordering::Equal => {
-                println!(" You WIN ");
+                println!("You Win");
                 break;
             }
-        }
     }
+
+    }
+
 }
